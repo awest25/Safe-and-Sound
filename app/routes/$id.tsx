02701eaf87d791f9eebe74422f1a6e4f1f9d0521
Alex West <awest2025@ucla.edu>
2024-04-21 05:51:55 -0700

    Added Airbnb Embed

--- app/routes/$id.tsx
+++ app/routes/$id.tsx
@@ -4,11 +4,13 @@ import { useLoaderData } from '@remix-run/react';
 import connectToDatabase from '../utils/mongodb.js';
 import { ObjectId } from 'mongodb';
 import DangerList from '~/components/DangerList';
+import AirbnbWidget from '~/components/AirbnbWidget.js';
 
 export const loader: LoaderFunction = async ({ params }) => {
   const client = await connectToDatabase();
   const db = client.db("safe-and-sound");
   let listing = await db.collection('airbnb_full').findOne({ _id: new ObjectId(params.id) });
+  // console.log(listing);
   if (!listing) {
     throw new Response('Not Found', { status: 404 });
   }
@@ -20,10 +22,13 @@ export default function AirbnbListing() {
   const listing = loaderData.listing;
   
   return (
-    <div>
-      <h1>{listing.name}</h1>
-      <DangerList propertyDocument={listing} />
-      {/* Render other details of your listing */}
+    <div style={{display: 'flex', flexDirection: 'row'}}>
+      <AirbnbWidget listing={listing} />
+      <div>
+        <h1>{listing.name}</h1>
+        <a href={listing.listing_url}>View Listing</a>
+        <DangerList propertyDocument={listing} />
+      </div>
     </div>
   );
 };
