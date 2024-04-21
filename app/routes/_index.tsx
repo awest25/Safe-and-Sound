35e053108136678b6848de53bee60813bb40a0b0
Alex West <awest2025@ucla.edu>
2024-04-21 06:49:13 -0700

    Added neighborhoods_la data

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -39,6 +39,12 @@ export const loader: LoaderFunction = async () => {
   const crimeData = await db.collection("crime_la").find({}, {projection: {_id: 1, location: 1}}).limit(10000).toArray();
   console.log("Successfully pulled crime data....")
 
+  const neighborhoodsData = (await db.collection("neighborhoods_la")
+                                     .find({}, {projection: {_id: 1, geometry: 1}})
+                                     .limit(10000).toArray())
+                                     .map((polygon) => polygon.geometry);
+  console.log("Successfully pulled neighborhood data....")
+
   // Create aggregation pipeline to get the concentration data
   const concentrationPipeline = [
     {
@@ -67,37 +73,12 @@ export const loader: LoaderFunction = async () => {
   console.log(`Successfully pulled ${concentrationData.length} concentration data points....`)
 
   console.log("Successfully queried all data 🎉")
-  // Inside your loader function
-  const polygonsSampleData = [{
-    type: 'Polygon',
-    coordinates:  [[
-      [-67.13734351262877, 45.137451890638886],
-      [-66.96466, 44.8097],
-      [-68.03252, 44.3252],
-      [-69.06, 43.98],
-      [-70.11617, 43.68405],
-      [-70.64573401557249, 43.090083319667144],
-      [-70.75102474636725, 43.08003225358635],
-      [-70.79761105007827, 43.21973948828747],
-      [-70.98176001655037, 43.36789581966826],
-      [-70.94416541205806, 43.46633942318431],
-      [-71.08482, 45.3052400000002],
-      [-70.6600225491012, 45.46022288673396],
-      [-70.30495378282376, 45.914794623389355],
-      [-70.00014034695016, 46.69317088478567],
-      [-69.23708614772835, 47.44777598732787],
-      [-68.90478084987546, 47.184794623394396],
-      [-68.23430497910454, 47.35462921812177],
-      [-67.79035274928509, 47.066248887716995],
-      [-67.79141211614706, 45.702585354182816],
-      [-67.13734351262877, 45.137451890638886]
-    ]]
-  }];
+
   return json({ 
     airbnb: airbnbData,
     crime: crimeData,
     concentration: concentrationData,
-    polygons: polygonsSampleData
+    polygons: neighborhoodsData
   });
 };
 
@@ -306,6 +287,18 @@ export default function Index() {
     }
   }
 
+  const neighborhoodLayer: Layer = {
+    id: 'neighborhoods',
+    type: 'line', // Changed from 'fill' to 'line' to represent the dividing lines
+    source: 'neighborhoods',
+    layout: {},
+    paint: {
+      'line-color': '#088', // Color of the line
+      'line-width': 2,     // Width of the line in pixels
+    }
+  };
+  
+
   return (
     <div className="h-screen">
       <div className="flex justify-center p-6 title"> 
@@ -341,14 +334,7 @@ export default function Index() {
             <Layer {...concentrationHeatLayer} layout={{ visibility: showConcentration ? 'visible' : 'none' }} />
           </Source>
           <Source type="geojson" data={polygonsData}>
-            <Layer
-              id="polygons"
-              type="fill"
-              paint={{
-                'fill-color': '#088',
-                'fill-opacity': 0.8
-              }}
-            />
+            <Layer {...neighborhoodLayer} />
           </Source>
           
         </Map>
